f5b79beebc296865a04761f13cf39a7e4b3b6f3f
Ariana <152944814+aam214@users.noreply.github.com>
2024-10-06 19:35:34 -0500

    Changed id and class name for input element. Created variables for generateButton and inputText. Used a click addEventListener for the generateButton to getPassword( ). Made a getPassword function to generate a randomNumber and password. Used inputTex.value to show the password in the input box.

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const generateButton = document.querySelector(".generate-button");
+const inputText = document.getElementById("input-text");
 
 generateButton.addEventListener("click", () => {
 getPassword();
 })
 
-let getPassword = () => {
+function getPassword() {
 const characters = 
 "abcdefghi1234567890ABCDEFGHIJK!@#$%&*";
 const maxLength = 12;
 let password = "";
 for (let index = 0; index < maxLength; index++) {
-const randomNum = 
-Math.floor(Math.random() * characters.length);
+
+const randomNum = Math.floor(Math.random() * characters.length);
 password += characters.substring(randomNum, randomNum + 1);
+//console.log(password);
+
 }
+inputText.value= password;
+
 }
