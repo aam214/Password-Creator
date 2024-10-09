cf1772b242851bafa33f882107448cd9e729fc65
Ariana <152944814+aam214@users.noreply.github.com>
2024-10-08 20:47:45 -0500

    Removed HTML text in alert message. Edited styling of alert message and .active.alert-message. Changed querySelector to getElementById for generate-button. Added alertMessage.classList, setTimeout, and if/else statement to copyIcon event listener. Added more characters to the characters variable. Created copyNow function to highlight inputText and copy it to the clipboard.

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-const generateButton = document.querySelector(".generate-button");
+const generateButton = document.getElementById("generate-button");
 const inputText = document.getElementById("input-text");
+const copyIcon = document.querySelector(".far.fa-copy");
+const alertMessage= document.querySelector(".alert-message");
 
 generateButton.addEventListener("click", () => {
 getPassword();
-})
+});
+
+copyIcon.addEventListener("click", () => {
+  copyNow();
+  if(inputText.value){
+  alertMessage.classList.remove("active");
+  setTimeout(() => {
+    alertMessage.classList.add("active");
+  }, 2500);
+}
+});
 
 function getPassword() {
 const characters = 
-"abcdefghi1234567890ABCDEFGHIJK!@#$%&*";
+"abcdefghi1234567890ABCDEFGHIJK!@#$%&^=+*";
 const maxLength = 12;
 let password = "";
 for (let index = 0; index < maxLength; index++) {
-
 const randomNum = Math.floor(Math.random() * characters.length);
 password += characters.substring(randomNum, randomNum + 1);
 //console.log(password);
-
 }
 inputText.value= password;
+alertMessage.innerText = "Copied: " + password;
+}
 
+function copyNow() {
+  inputText.select();
+  inputText.setSelectionRange(0, 9999);
+  navigator.clipboard.writeText(inputText.value);
 }
